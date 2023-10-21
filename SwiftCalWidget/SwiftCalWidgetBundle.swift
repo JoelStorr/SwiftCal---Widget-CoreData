68205e1d2dfbd81fb3e1301654da3472d03a7508
Joel Storr <joel.storr@ewe.net>
2023-10-21 16:38:27 +0200

    Added widget template to app.

---
+++ SwiftCalWidget/SwiftCalWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftCalWidgetBundle.swift
+//  SwiftCalWidget
+//
+//  Created by Joel Storr on 21.10.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SwiftCalWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SwiftCalWidget()
+    }
+}
