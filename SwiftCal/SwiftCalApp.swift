16a95ca11f2ed3b18865bed49dafdad2c6d66c61
Joel Storr <joel.storr@ewe.net>
2023-10-21 22:04:51 +0200

    Added Touch Targets to widget

--- SwiftCal/SwiftCalApp.swift
+++ SwiftCal/SwiftCalApp.swift
@@ -11,16 +11,23 @@ import SwiftUI
 struct SwiftCalApp: App {
     let persistenceController = PersistenceController.shared
 
+    @State private var selectedTab = 0
+    
     var body: some Scene {
         WindowGroup {
             
-            TabView{
+            TabView(selection: $selectedTab ){
                 CalendarView()
                     .tabItem { Label("Calendar", systemImage: "calendar") }
+                    .tag(0)
                 StreakView()
                     .tabItem { Label("Streak", systemImage: "swift") }
+                    .tag(1)
             }
             .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            .onOpenURL(perform: { url in
+                selectedTab = url.absoluteString == "calendar" ? 0 : 1
+            })
         }
     }
 }
